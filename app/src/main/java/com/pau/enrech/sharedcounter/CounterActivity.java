a14220927f4b45410ab390e9e9e35ff1e45b4712
Penrech <pauenrech94@gmail.com>
2018-04-10 10:35:53 +0200

    solo el layout

---
+++ app/src/main/java/com/pau/enrech/sharedcounter/CounterActivity.java
@@ -0,0 +1,30 @@
+package com.pau.enrech.sharedcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class CounterActivity extends AppCompatActivity {
+
+    private TextView counter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_counter);
+
+        counter = findViewById(R.id.counter);
+    }
+
+    public void sumarUn(View view){
+
+    }
+
+    public void restarUn(View view){
+
+    }
+    public void reset(View view){
+
+    }
+}
